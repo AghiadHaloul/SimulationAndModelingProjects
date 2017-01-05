cf4ff3b5433c0e9675332983d7be3ac7cdd0fbb5
Aghiad Haloul <aghiadx@gmail.com>
2017-01-05 17:36:49 +0200

    RandomValue Class

---
+++ Task 1 Multi-Channel Queue/MultiChannelQueuing/RandomValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiChannelQueuing
+{
+    public class RandomValue
+    {
+        static Random r = new Random();//For randomly selecting the server.
+        public static int getRandom(int min, int max)
+        {
+            r = new Random();
+            return r.Next(min, max);
+        }
+    }
+}
